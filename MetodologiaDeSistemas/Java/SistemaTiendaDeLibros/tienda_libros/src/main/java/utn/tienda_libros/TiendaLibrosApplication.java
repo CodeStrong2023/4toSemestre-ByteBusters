c115ab3c4518b7c6c33dea2c1f933eec0355cc46
Emiliano Grossi <emigrossi2004@gmail.com>
2024-10-07 10:39:53 -0300

    Clase 08 Proyecto Tienda de Libros con SpringBoot y Swing -> Parte 03

--- MetodologiaDeSistemas/Java/SistemaTiendaDeLibros/tienda_libros/src/main/java/utn/tienda_libros/TiendaLibrosApplication.java
+++ MetodologiaDeSistemas/Java/SistemaTiendaDeLibros/tienda_libros/src/main/java/utn/tienda_libros/TiendaLibrosApplication.java
@@ -1,13 +1,26 @@
 package utn.tienda_libros;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class TiendaLibrosApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(TiendaLibrosApplication.class, args);
+		ConfigurableApplicationContext contextoSpring = 
+			new SpringApplicationBuilder(TiendaLibrosApplication.class)	
+				.headless(false)
+				.web(WebApplicationType.NONE)
+				.run(args);
+
+		java.awt.EventQueue.invokeLater(() ->{//Metodo Lambda
+			//Obtenemos el objeto form a travez del Sping
+			LibroForm libroForm = contextoSpring.getBean(LibroForm.class);
+			libroForm.setVisible(true);
+		});
 	}
 
 }
