79a8f8c337984439a021d4d0eaa120785e5fbcb6
Andres Luna <andresluna201@hotmail.com>
2024-11-10 14:57:09 -0300

    añadi clases 7,8 y 9 de python. Añadi un video al inicio, 3 enemigos y musica nueva de fondo

--- LaboratorioIV/Python/clase7-8-9-pyGame StarTrek/constantes.py
+++ LaboratorioIV/Python/clase7-8-9-pyGame StarTrek/constantes.py
@@ -9,3 +9,6 @@ COLOR_LASER = (0, 0, 255)  # Azul
 
 # Ruta a los assets
 ASSETS_PATH = os.path.join(os.path.dirname(__file__), 'assets_1')
+
+#Rutas de los archivos
+MARCH_PATH = f'{ASSETS_PATH}/sound/inicio.mp3'
